c7174f2fc173fe6904d170cc671deb3a7b86a758
clairemuiru1 <muiruclaire@gmail.com>
2023-10-15 14:15:00 +0300

    "Completed assignment"

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+      .then((response) => response.json())
+      .then((json)=> {renderBooks(json)});
   
+      
 }
 
 function renderBooks(books) {
@@ -8,9 +12,16 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
+
+fetch(`https://anapioficeandfire.com/api/books/${bookId}`)
+     .then( response => response.json())
+     .then((data) =>{
+      
+     })
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
